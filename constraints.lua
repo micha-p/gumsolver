277fe8fba779523dec3fc06cf1d6b66c9c0eccde
work <work@localhost>
2013-06-26 18:11:23 +0200

    calling entries instead of sending signals to functions

--- constraints.lua
+++ constraints.lua
@@ -1,64 +1,40 @@
-function cons(a, b) return {car=a, cdr = b} end
-function car (x)    return x.car end
-function cdr (x)    return x.cdr end
-
-function member (a, list)
-  if list == nil then return nil 
-  elseif a == car (list) then return not nil 
-  else return member (a ,cdr (list))
+function table.find (t, value)
+  for k,v in pairs(t) do
+    if v==value then return k end
   end
+  return nil
 end
 
-function for_each_except (exception, procedure, list)
-  local function loop (items)
-    if nil == items then return
-    elseif car (items) == exception then loop (cdr (items))
-    else procedure (car (items))
-         loop (cdr (items))
-    end
-  end
-  loop (list)
-end
 
 -------------------------------------------------------------------------
 -------------------------------------------------------------------------
 
-value = 1
-get = 2
-set = 3
-forget = 4
-connect = 5
-new = 10
-lost = 11
-
-function actor (process_new_value, process_forget_value)  
-  local me
-  me = function (request)
-             if request == new then process_new_value()
-             elseif request == lost then process_forget_value()
-             end
-           end
+function make_actor (process_new_value, process_forget_value)  
+  local me = {}
+  me.new  = function () process_new_value() end
+  me.lost = function () process_forget_value() end
   return me
 end
 
 function constraint (a, b, c, forward , back)  
-  local me
+  local me = {}
   local function process_new_value ()
-    if     a(value) and b(value) then c(set, me, forward (a(get), b(get))) 
-    elseif a(value) and c(value) then b(set, me, back    (c(get), a(get))) 
-    elseif b(value) and c(value) then a(set, me, back    (c(get), b(get))) 
+    if     a.value() and b.value()then c.set(me, forward (a.get(), b.get())) 
+    elseif a.value() and c.value()then b.set(me, back    (c.get(), a.get())) 
+    elseif b.value() and c.value()then a.set(me, back    (c.get(), b.get())) 
     end
   end
   local function process_forget_value ()
-    a(forget, me)
-    b(forget, me)
-    c(forget, me)
+    a.forget(me)
+    b.forget(me)
+    c.forget(me)
     process_new_value()
   end
-  me = actor (process_new_value, process_forget_value) 
-  a(connect, me)
-  b(connect, me)
-  c(connect, me)
+  me = make_actor (process_new_value, process_forget_value) 
+  a.connect(me)
+  b.connect(me)
+  c.connect(me)
+  return me
 end
 
 function adder     (a1, a2, sum)  constraint(a1,a2,sum , function(a,b) return a+b end, function(a,b) return a-b end) end
@@ -66,73 +42,60 @@ function subtractor(a1, a2, diff) constraint(a1,a2,diff, function(a,b) return a-
 function multiplier(m1, m2, prod) constraint(m1,m2,prod, function(a,b) return a*b end, function(a,b) return a/b end) end
 function divider   (a1, a2, sum)  constraint(a1,a2,sum , function(a,b) return a/b end, function(a,b) return a*b end) end
 
---[[ syntactic sugar  
-
-arrow is also used in OCaml, maple, f#, erlang, haskell, oft aber auch für typenübergabe
--> ist ein binärer operator in der Reihenfolge direkt über =
-
-adder      = (a1, a2, sum)  -> constraint(a1,a2,sum , (a,b) -> return a+b, (a,b) -> return a-b)
-subtractor = (a1, a2, diff) -> constraint(a1,a2,diff, (a,b) -> return a-b, (a,b) -> return a+b)
-multiplier = (m1, m2, prod) -> constraint(m1,m2,prod, (a,b) -> return a*b, (a,b) -> return a/b)
-divider    = (a1, a2, sum)  -> constraint(a1,a2,sum , (a,b) -> return a/b, (a,b) -> return a*b)
-
-]]
-
-
 
 function constant (value, connector) 
-  local me
-  me = actor () 
-  connector (connect, me)
-  connector (set, me, value)
+  local me = {}
+  me = make_actor () 
+  connector.connect(me)
+  connector.set(me, value)
+  return me
 end
 
 function probe (name, connector)
-  local me
-  local print_probe = function (value)
-    print ("Probe: ", name, " = ", value)
+  local me = {}
+  local printprobe = function (value)
+    print (name, " = ", value)
   end
-  me = actor (function () print_probe (connector (get)) end, function () print_probe ("?") end)
-  connector (connect, me)
+  me = make_actor (function () printprobe (connector.get()) end, function () printprobe ("?") end)
+  connector.connect(me)
+  return me
 end
 
 
 function make_connector()
-  local me 
-  local myval = nil
+  local me = {}
+  local value = nil
   local informant = nil
-  local constraints = nil
+  local constraints = {}
 
   local set_my_value = function (setter, newval)
-    if (not informant) then   -- informant = me(value)
-      myval = newval
+    if (not informant) then 
+      value = newval
       informant = setter
-      for_each_except (setter, function (constraint) constraint (new) end, constraints)
-    elseif myval ~= newval then print ("Contradiction" , myval , newval) -- myval = me(get)
+      table.foreach (constraints, function (k, v) if v ~= setter then v.new() end end)
+    elseif value ~= newval then print ("Contradiction" , value , newval)
     end
   end
 
   local forget_my_value = function (retractor)
     if retractor == informant then
        informant = nil
-       for_each_except (retractor, function (constraint) constraint (lost) end, constraints)
+       table.foreach (constraints, function (k, v) if v ~= retractor then v.lost() end end)
     end
   end
   
   local connect_actor = function (new_constraint)
-    if not member (new_constraint , constraints) then constraints = cons (new_constraint , constraints) end
-    if informant then inform_about_value (new_constraint) end
+    if not table.find (constraints, new_constraint) then table.insert (constraints, new_constraint) end
+    if informant then new_constraint.new() end
   end
 
-  me = function (request, actor, newval)
-              if request == value then return informant
-              elseif request == get then return myval 
-              elseif request == set then set_my_value (actor, newval)
-              elseif request == forget then forget_my_value (actor)
-              elseif request == connect then connect_actor (actor)
-              end
-            end
-   return me
+  me.value  = function () return informant end
+  me.get    = function () return value end
+  me.set    = function (actor, new) set_my_value    (actor, new) end
+  me.forget = function (actor)      forget_my_value (actor)      end
+  me.connect= function (actor)      connect_actor   (actor)      end
+
+  return me
 end
 
 
@@ -182,13 +145,12 @@ probe ("Fahrenheit ", F)
 probe ("Reaumur    ", R)
 probe ("Kelvin     ", K)
 
-C (set, "user", 25)
-F (set, "user", 212)
-C (forget, "user")
-F (set, "user", 212)
-F (forget, "user")
-R (set, "user", 80)
-R (forget, "user", 80)
-K (set, "user", 0)
-
+C.set("user", 25)
+F.set("user", 212)
+C.forget("user")
+F.set("user", 212)
+F.forget("user")
+R.set("user", 80)
+R.forget("user")
+K.set("user",0)
 
