2a8caebe73f63860a71fb29190e5e683f865f22c
work <work@localhost>
2013-06-27 17:43:49 +0200

    parsing formulas in tables
    TODO: fix mismatch between columnnames in header with formalua and fileds in records without formula

--- constraints_with_values.lua
+++ constraints_with_values.lua
@@ -1,11 +1,14 @@
 dofile ("constraints.lua")
 dofile ("values.lua")
 dofile ("csv.lua")
-
+dofile ("parser.lua")
 
 init_algebra (vadd,vsub,vmul,vdiv)
 init_print   (function (v)   return v.abs () end) 
 init_equal   (function (a,b) return a.v == b.v and a.D2 == b.D2 end) 
+
+
+c={}  -- global connector table
 
 function run (connectortable, c, val , abs , rel) 
    if c then
@@ -22,25 +25,63 @@ end
 function process_record (c, rec)
    run(c)
    for name,connector in pairs(c) do 
-      -- print ("Process ", name, "with value:" , rec[name])
       run (c, connector, rec[name], rec[name.."+-"] or rec[name.."±"], rec[name.."%"] and rec[name.."%"]/100) 
    end
 end
 
-c={}  -- global connector table
-c["w"] = make_connector()
-c["h"] = make_connector()
-c["area"] = cmul (c["w"],c["h"])
+function process_and_print (c, rec)
+   run(c)
+   for name,connector in pairs(c) do 
+      run (c, connector, rec[name], rec[name.."+-"] or rec[name.."±"], rec[name.."%"] and rec[name.."%"]/100) 
+   end
+   
+end
 
 
+function process_formula(formula)
+   local function apply (op1, infix, op2)
+   return infix=="+" and cadd (op1, op2)
+          or
+          infix=="-" and csub (op1, op2)
+          or
+          infix=="*" and cmul (op1, op2)
+          or
+          infix=="/" and cdiv (op1, op2)
+   end
 
+   local function eval(node)
+   return stringtest(node) and c[node]
+          or
+          numbertest(node) and cv(vnew(node))
+          or
+          tabletest(node) and apply (eval(node[1]),node[2],eval(node[3])) 
+   end 
+
+   name=formula:match("%s*(.+)%s*=")
+   expr=formula:match(".*=(.*)$")
+   c[name]=eval(parse(expr))
+return not nil
+end
+    
+function process_column(colname)
+   local function new()
+      c[colname]=make_connector()
+      return nil
+   end
+return colname:match("%+%-$") 
+	or 
+   	colname:match("±$") 
+   	or 
+   	colname:match ("%%$")
+   	or 
+   	colname:match ("=") and process_formula(colname)
+   	or
+   	new()
+end
+
+
+r,h=csv_read("tableformula.txt","\t")
+
+for col, colname in ipairs(h) do process_column(colname) end 
 for name,connector in pairs(c) do print("Field:",name); probe(name,connector) end
-r=csv_read("table.csv",",")
-
-
 for line, record in ipairs(r) do process_record (c, record) ; print ("===============") end
-
-
-
-
-
---
+++ parser.lua
@@ -0,0 +1,116 @@
+dofile("display.lua")
+
+--[[
+
+V 0.2 operator precedence
+V 0.1 init
+
+]]
+
+
+
+function parse (str) 
+   local function expression (str, pos, len, level)
+      local e={}
+      local i=1
+   
+      local function check_pattern (pattern)
+         if pos > len then 
+            return nil
+         else
+            m = string.match (str,pattern, pos)
+            if m then pos = pos + #m end
+            return m
+         end
+      end
+
+      local function number() 
+         m = check_pattern ("^-?[0-9%.]+") 
+         return m and assert(tonumber(m), "Error while converting to number: " .. m or "nil")
+      end
+      local function subexpression()
+         local m = check_pattern ("^%(") 
+         if m then 
+            s, newpos = expression (str, pos, len, level+1)
+            pos=newpos
+         end
+         return s
+      end
+   
+      local function skip_space() check_pattern ("%s*") end
+      local function variable() return check_pattern ("^[a-zA-Z][a-zA-Z0-9_]*") end  
+      local function closing() return check_pattern ("^%)") end  
+      local function operator() return check_pattern ("^[%+%-%*%/]") end  
+      local function operand() return variable() or number() or subexpression() or nil end
+   
+   
+      skip_space()
+      e[i]=operand()
+      skip_space()
+      for a in operator do
+         i=i+1
+         e[i]=a
+         skip_space()
+         i=i+1
+         e[i]=operand()
+         skip_space()
+      end
+      if level>1 and not closing() then error("closing bracket missing before end") end
+      if level==1 and closing() then error("too many closing brackets") end
+      return e, pos
+   end
+   return expression (str,1, #str,1)
+end
+
+function uncurry (expr)
+   if tabletest(expr) and #expr > 3 then
+      local r={}
+      r[1]=expr[1]
+      r[2]=expr[2]
+      r[3]=expr[3]
+      table.remove(expr,3)
+      table.remove(expr,2)
+      expr[1]=r
+      return uncurry(expr)
+   else
+      return expr
+   end
+end
+
+
+
+function find_first_highest_rank (expr)
+   rank={["+"]= 1,["-"]= 1,["*"]= 2,["/"]= 2}
+   display (rank)     
+   local pos=2
+   for i,v in ipairs (expr) do
+      print (i,v,rank[v],rank[expr[pos]])
+      if math.mod(i, 2) == 0 and rank[v] > rank[expr[pos]] then pos=i end
+   end
+return pos
+end
+
+
+function order (expr)
+   if tabletest(expr) and #expr > 3 then
+      local r={}
+      local h=find_first_highest_rank (expr)
+      r[1]=expr[h-1]
+      r[2]=expr[h]
+      r[3]=expr[h+1]
+      table.remove(expr,h+1)
+      table.remove(expr,h)
+      expr[h-1]=r
+      return order(expr)
+   else
+      return expr
+   end
+end
+
+--[[
+display (uncurry (parse ("a+b+c+d+d")))
+display (uncurry (parse ("a+b+c*d+d")))
+display (find_first_highest_rank (parse ("a+b+c+d+d")))
+display (order (parse ("a+b+c*d+d")))
+display (order (parse ("a+b*c/d")))
+]]
