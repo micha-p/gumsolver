6eb600ceb5922e96478d6e00d0c14f7b39391075
micha-p <michael.pfiz@yahoo.com>
2013-08-08 13:43:30 +0200

    Preparations for release 0.2

---
+++ dump.lua
@@ -0,0 +1,147 @@
+RECORDS = {}
+
+function print2stderr (name, value)
+    warn (PRINT16(name), value and tostring(value) or ".")
+end
+
+function probe2stderr (name, connector)
+   local me = {}
+   local actors = {connector}
+   me = make_actor (function () print2stderr (name, PRINT (connector.get())) end, function () print2stderr (name,".") end)
+   connector.connect(me)
+   me["class"]  = "probe"
+   me["setters"]  = function () return actors end
+   return me
+end
+
+function print_record()
+   local r={}
+   local sorted={}
+   
+   for k,v in pairs(CONNECTORS) do table.insert(sorted,k) end
+   table.sort(sorted)
+   
+   for i,n in ipairs(sorted) do
+      v=CONNECTORS[n] 
+      if TABLE or RECORD then 
+         if DEBUG then print2stderr (PRINT16(n), v.value() and PRINT(v.get()) or "") end
+      else
+         if not MUTE then print (PRINT16(n), v.value() and PRINT(v.get()) or "") end
+      end
+   end
+end
+
+
+function save_record()
+   local r={}
+   for k,v in pairs(CONNECTORS) do 
+      r[k]=v.value() and v.get()
+   end
+   table.insert(RECORDS,r)
+end
+
+function clear_record()
+   local old = MUTE
+   MUTE=not nil
+   run(CONNECTORS) --> clear
+   MUTE= old
+end
+
+function record_connectors()
+   print_record()
+   save_record()
+   clear_record()
+return not nil   
+end
+
+function tabulate_records()
+   local colnames={}
+   local headers={}
+  
+   for k,v in pairs (CONNECTORS) do 
+      table.insert(colnames,k) 
+      table.insert(headers,DEFINITIONS[k] and k.."="..DEFINITIONS[k] or k) 
+      table.insert(headers,RELATIVE and k.."%" or k.."±") 
+   end
+   
+   print(unpack(headers))
+   for line, record in ipairs(RECORDS) do 
+      local r={}
+      for k,v in pairs (colnames) do 
+         table.insert(r, PRINTV(record[v])) 
+         table.insert(r, PRINTE(record[v])) 
+      end
+      print(unpack(r))
+   end
+end
+
+
+function dump_connectors()
+   function name(k)  io.write(PRINT16(k),"\t") end
+   function left(v)  io.write (short(v), "\t") end
+   function printconnections(ltable)
+      local out={}   
+      for k,l in pairs(ltable) do 
+         info=l["info"] and l.info() 
+         if DEBUG then 
+            table.insert(out, informant == l and "!" or "")
+            table.insert(out, short(l))
+            table.insert(out, "=")
+            table.insert(out, info or "")
+         else
+            table.insert(out, info or ".")
+         end   
+         table.insert(out, " ")
+      end
+   io.write (string.format(DEBUG and "%-48.47s" or "%-24s",table.concat(out)))
+   end
+
+   function right(v)
+      if v["value"] then  
+         informant=(v.value())
+         io.write (informant and PRINT(v.get()) or "", stringtest(informant) and " ("..informant..")" or "","\n")
+      else
+         print()
+      end
+   end   
+      
+   local keys={}
+   for k,v in pairs(CONNECTORS) do table.insert(keys, k) end
+   table.sort(keys) 
+   
+   print("SYMBOLS (CONNECTORS):")
+   for k,v in pairs (DEBUG and CONNECTORS or keys) do
+      if DEBUG then left (v); name(k) else name(v); v=CONNECTORS[v] end
+      ltable=  v["listeners"] and (v.listeners()) or {}
+      printconnections(ltable)
+      right(v)
+   end
+   print("SUBEXPRESSIONS (CONNECTORS):")
+   for k,v in pairs (SUBEXPRESSIONS) do
+      if DEBUG then left (v) end
+      name(k)
+      informant=(v.value())
+      ltable=(v.listeners())
+      printconnections(ltable)
+      right(v)
+   end
+   print("CONSTRAINTS:")
+   for k,v in pairs (CONSTRAINTS) do
+      if DEBUG then left (v) end
+      name(k)
+      setters=(v.setters())
+      printconnections(setters)
+      print ()
+   end
+
+   if DEBUG then
+      print("PROBES:")
+      for k,v in pairs (PROBES) do
+      if DEBUG then left (v) end
+         name(k)
+         setters=(v.setters())
+         printconnections(setters)
+         print ()
+      end
+   end
+end
---
+++ expressions.lua
@@ -0,0 +1,72 @@
+require 'constraints_with_values'
+require 'parser'
+
+function run (connectortable, connector, val , abs , rel) 
+   if connector then
+      if val then 
+         connector.set ("user", tabletest (val) and val or vnew (val, abs, rel))
+      else
+         connector.forget ("user")
+      end
+   else
+      for name,c in pairs(connectortable) do c.forget ("user") end
+   end
+end
+
+function EVAL(createfunc, expr)
+   local function apply (op1, infix, op2)
+   return infix=="+" and cadd (op1, op2)
+          or
+          infix=="-" and csub (op1, op2)
+          or
+          infix=="*" and cmul (op1, op2)
+          or
+          infix=="/" and cdiv (op1, op2)
+   end
+return stringtest(expr) and createfunc(expr)
+       or
+       numbertest(expr) and cval (vnew(expr))
+       or
+       tabletest(expr)  and apply (EVAL(createfunc,expr[1]),expr[2],EVAL(createfunc,expr[3])) 
+       or
+       error ("Can't resolve expression: "..node)
+end 
+
+function process_expr (createfunc, formula)
+   name=extract_name(formula)
+   expr=extract_expr(formula)
+   c[name] = EVAL(createfunc, order(parse(expr)))
+   probe(name,c[name])
+end
+
+--[[
+
+c={} 
+
+function create(name) 
+   if not c[name] then
+      c[name]=make_connector()
+      probe(name,c[name])
+   end
+return c[name]
+end
+
+process_expr (create,"F = ( (9 / 5 ) * C ) + 32" )
+process_expr (create,"K = C + 273.15")
+process_expr (create,"R = 80 * (C / 100)")
+
+run(c,c["C"], 25)
+run(c,c["F"], 212)
+run(c,c["C"])
+run(c,c["F"], 212)
+run(c,c["F"])
+run(c,c["K"],0)
+run(c,c["K"])
+run(c,c["R"], 80)
+run(c,c["R"])
+run(c,c["R"], 0)
+run(c,c["R"])
+run(c,c["C"], 100) 
+
+--]]
+
